fceda7b0180d67f41cb693910fb41c2057121b31
Vignesh21J <vignesh11212003@gmail.com>
2025-11-17 17:52:53 +0530

    feat: React HomePage UI

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer my-3 py-3'>
+            <hr className='border-bottom' />
+            <p className='text-light text-center'>&copy; 2025 - Built with 💜 by Vignesh</p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
