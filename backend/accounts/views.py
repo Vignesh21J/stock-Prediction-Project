0cec8d488ebad35a94fa15131aeb89147ab3d046
Vignesh21J <vignesh11212003@gmail.com>
2025-11-18 12:28:52 +0530

    feat: registration backend/api

---
+++ backend/accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    permission_classes = [AllowAny]
--- backend/sp_main/settings.py
+++ backend/sp_main/settings.py
@@ -29,6 +29,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'accounts',
+    'api',
 ]
 
 MIDDLEWARE = [
